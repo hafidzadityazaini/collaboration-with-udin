4462c90e858c981e3635589907a8f27ebda3e996
HanzxRYU <rafi0315@abudzar.sch.id>
2025-05-05 09:44:08 +0700

    uddin

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -9,15 +9,25 @@ function App() {
         <header className="text-3xl font-bold text-gray-800 mb-6">
           Project collaboration with Adit
         </header>
-        <h1 className="text-3xl font-bold text-gray-800 mb-6">Punya </h1>
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Punya Adit</h1>
         <img
           src="Gpt.png"
           alt="Collaboration project"
           className="rounded-2xl shadow-lg w-full"
         />
       </section>
-
-      
+      {/* Section 2 */}
+      <section className="w-full max-w-3xl flex flex-col items-center">
+        <header className="text-3xl font-bold text-gray-800 mb-6">
+          Project collaboration with Udin's
+        </header>
+        <h1 className="text-3xl font-bold text-gray-800 mb-6">Punya Udin</h1>
+        <img
+          src="freaky.jpg"
+          alt="Collaboration project"
+          className="rounded-2xl shadow-lg w-full"
+        />
+      </section>
     </div>
   );
 }
